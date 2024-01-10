71c0ddef815c6b92600f228bfd5e60360e766e63
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-01-10 03:04:38 +0100

    Added the new live API

--- js/common/commons.js
+++ js/common/commons.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = `http://game-hub.local/`;
+const API_BASE_URL = `https://game-hub.berremarte.no/`;
 
 export const GAMEHUB_API_URL = `${API_BASE_URL}/wp-json/wc/store/products`;
